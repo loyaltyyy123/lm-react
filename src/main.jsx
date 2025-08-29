2f9c530aba6c95f57b8bbe8933e732e2d79898bb
loyaltyyy123 <pranatrhireddy9928@gmail.com>
2025-08-29 15:55:35 +0530

    message

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Hello from './heelo.jsx';
+import Hello from './Hello.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
